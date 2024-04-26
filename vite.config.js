b8b053156832b0adfeadbb09c2cb34e4d2bef1a5
apolline-diaz <apolline.diaz@gmail.com>
2024-04-26 12:16:53 +0200

    Update Base in vite.config.js for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Movie-App/",
+  base: "/",
 })
 
 // pour déployer : https://medium.com/@aishwaryaparab1/deploying-vite-deploying-vite-app-to-github-pages-166fff40ffd3
