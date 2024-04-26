565065509dc15cc2458874aea93c3cfc0da7affa
apolline-diaz <apolline.diaz@gmail.com>
2024-04-26 13:28:27 +0200

    Redefine Movie-App in app.jsx and vite.config.js and package.json for Git Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/movie-app/",
+  base: "/Movie-App/",
 })
 
 // pour déployer : https://medium.com/@aishwaryaparab1/deploying-vite-deploying-vite-app-to-github-pages-166fff40ffd3
