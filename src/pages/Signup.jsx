78d5ee257a147f1b67b51bd8f0ce18a8f5887ccb
apolline-diaz <apolline.diaz@gmail.com>
2024-06-25 11:27:17 +0200

    Sign Up Page New Design similar to Sign In

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,29 +7,29 @@ const SignUp = () => {
     <>
       <div className='w-full h-screen'>
         <img
-          className='hidden sm:block absolute w-full h-full object-cover'
+          className='absolute w-full h-full object-cover'
+          // hidden sm:block
           src={Background}
           alt='/'
         />
-        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
-        <div className='fixed w-full px-4 py-24 z-50'>
-          <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
-            <div className='max-w-[320px] mx-auto py-16'>
+        <div className='bg-black/50 fixed top-0 left-0 w-full h-screen flex justify-center items-center'>
+          <div className='w-full max-w-[450px] h-auto bg-white text-black rounded-lg'>
+            <div className='max-w-[320px] mx-auto py-10'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
               <form className='w-full flex flex-col py-4'>
                 <input
-                  className='p-3 my-2 bg-gray-700 rounded'
+                  className='p-3 my-2  border border-2-black rounded-full'
                   type='email'
                   placeholder='Email'
                   autoComplete='email'
                 />
                 <input
-                  className='p-3 my-2 bg-gray-700 rounded'
+                  className='p-3 my-2  border border-2-black rounded-full'
                   type='password'
                   placeholder='Password'
                   autoComplete='current-password'
                 />
-                <button className='bg-pink-400 py-3 my-6 rounded font-bold'>
+                <button className='bg-pink-400 hover:bg-pink-300 py-3 my-6 rounded-full font-bold'>
                   Sign Up
                 </button>
                 <div className='flex justify-between items-center text-sm text-gray-600'>
